9b53dab34a0fe7dfec56e6aedf29ea104e76564f
Josh Moody <joshmoody24@gmail.com>
2022-10-03 21:52:48 -0600

    row coloring complete

--- Wordle.py
+++ Wordle.py
@@ -14,57 +14,55 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT
 def is_english(word):
     return word.lower() in FIVE_LETTER_WORDS
 
+def colorize_current_row(guess, answer, gw):
+    # this dictionary tracks the number of times each letter appears in the answer
+    word_counts = {}
+    for col in range(N_COLS):
+        letter = answer[col]
+        if(letter not in word_counts):
+            word_counts[letter] = 1
+        else:
+            word_counts[letter] += 1
+
+    # set them all to unknown color to start (just in case
+    for col in range(N_COLS):
+        gw.set_square_color(gw.get_current_row(), col, UNKNOWN_COLOR)
+    
+    # Turn exactly correct guesses green
+    for col in range (N_COLS):
+        guess_letter = guess[col]
+        if(guess_letter == answer[col] and word_counts[guess_letter] > 0):
+            gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
+            word_counts[guess_letter] -= 1
+
+    # Turn near misses yellow
+    for col in range(N_COLS):
+        guess_letter = guess[col]
+        if(guess_letter in answer and word_counts[guess_letter] > 0):
+            gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
+            word_counts[guess_letter] -= 1
+
 def wordle():
 
-    def enter_action(s):
-        gw.show_message("Valid english word!" if is_english(s) else "Not in word list")
-        
-        # Tells the user when they guessed the right word
-        if s == wordleWord:
-            gw.show_message("That's correct! Nice job!")
-        
-        # Set CORRECT_COLOR
-        for col in range (0, N_COLS):
-            if s[col] == wordleWord[col]:
-                gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
-        
-        # Set PRESENT_COLOR
-        for col in range(0, N_COLS):
-            # Check if letter color has already been filled out
-            if gw.get_square_color(gw.get_current_row(), col) == UNKNOWN_COLOR:
-
-                # FIX THIS FOR LOOP
-                # This is where I'm starting to have issues. The problem is that most cases work, but currently if you enter a word that 
-                # has two occurrences of a letter in the wordleWord both will be highlighted. 
-                # I've set the wordleWord to be "HELLO" every time for debugging so to see what I'm talking about type in "SHOOT".
-                # You'll see that both the "O"s highlight as yellow when only one of them should.
-
-                # Iterate through the wordle word to see if the current letter is found in it.
-                for wordleIndex in range(0, N_COLS):
-                    # If the current letter is found then assign it PRESENT_COLOR
-                    if s[col] == wordleWord[wordleIndex]: 
-                        gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
-                        print(copyWordleWord)
-                        break
-        
-        # NEXT STEP IS TO CHANGE THE ROW AFTER A USER ENTERS A GUESS
-
     gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-    
-    # Choose random word from WordleDictionary.py
-    displayWord = random.choice(FIVE_LETTER_WORDS)
-    # Set first row characters to the characters from displayWord
-    for col in range(0, N_COLS):
-        gw.set_square_letter(0, col, displayWord[col].upper())
-
-    # Sets the word the user is trying to guess!
-    # wordleWord = random.choice(FIVE_LETTER_WORDS)
-    wordleWord = "HELLO"
-    
+    answer = "HELLO"
     guessNum = 0
 
-# Startup code
+    def enter_action(guess):
+        if(" " in guess):
+            return
+        if(not is_english(guess)):
+            gw.show_message("Not in word list")
+            return
+        colorize_current_row(guess, answer, gw)
+        # Tells the user when they guessed the right word
+        if guess == answer:
+            gw.show_message("That's correct! Nice job!")
+        
+        gw.set_current_row(gw.get_current_row() + 1)
+    
+    gw.add_enter_listener(enter_action)
 
+# Startup code
 if __name__ == "__main__":
     wordle()
